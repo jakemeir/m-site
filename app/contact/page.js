7624e044894c3b7db908c7bbb47c7ccd7ce96be4
jakemeir <jakemeirjacobs@gmail.com>
2024-08-27 20:53:02 +0300

    fixed spelling And added form validation

--- app/contact/page.js
+++ app/contact/page.js
@@ -49,8 +49,8 @@ export default function ContactPage() {
               Message
             </label>
             <textarea
-              id="massage"
-              name="massage"
+              id="message"
+              name="message"
               rows="4"
               required
               className="border border-gray-600 bg-gray-900 text-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
