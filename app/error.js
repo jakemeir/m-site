1894d48cb1ed046b4ac3e0174a05db0a73897b31
jakemeir <jakemeirjacobs@gmail.com>
2024-08-27 23:02:32 +0300

    added error page

---
+++ app/error.js
@@ -0,0 +1,12 @@
+"use client";
+import Link from "next/link";
+
+export default function error() {
+  return (
+    <>
+      <p>Well this is Embarrassing</p>
+      <p>Please forget this happened. here you can go back to our home page</p>
+      <Link>HERE</Link>
+    </>
+  );
+}
