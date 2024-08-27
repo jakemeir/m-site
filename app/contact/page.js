2b787cadff3cecf6b720ba69626ba0d82f795aad
jakemeir <jakemeirjacobs@gmail.com>
2024-08-27 21:27:53 +0300

    Added clearer form validation

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,12 @@
 "use client";
 import submitHandler from "@/lib/submission";
+import { useFormState } from "react-dom";
 export default function ContactPage() {
+  const [state, formAction] = useFormState(submitHandler, { message: "" });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="max-w-2xl mx-auto p-4 bg-gray-800 shadow-md rounded-lg">
-        <form className="space-y-4" action={submitHandler}>
+        <form className="space-y-4" action={formAction}>
           <div className="flex flex-col space-y-1">
             <label htmlFor="company" className="text-gray-300 font-medium">
               Company
@@ -64,6 +66,7 @@ export default function ContactPage() {
             Submit
           </button>
         </form>
+        {state.message && <p>{state.message}</p>}
       </div>
     </main>
   );
---
+++ app/contact/thanks/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function ThankYou() {
+  return (
+    <>
+      <div>thank you for sharing </div>
+      <Link href="/">HOME</Link>
+    </>
+  );
+}
